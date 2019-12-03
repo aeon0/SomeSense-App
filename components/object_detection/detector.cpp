bfa3c232f36ebf5732b86a5167297b47eba2b24c
j-o-d-o <jdobler@protonmail.com>
2019-12-03 21:33:49 +0100

    add additional includes

--- components/object_detection/detector.cpp
+++ components/object_detection/detector.cpp
@@ -1,6 +1,12 @@
 #include "detector.h"
 #include <iostream>
 
+#include <cstdio>
+#include "tensorflow/lite/interpreter.h"
+#include "tensorflow/lite/kernels/register.h"
+#include "tensorflow/lite/model.h"
+#include "tensorflow/lite/optional_debug_tools.h"
+
 
 void object_detection::Detector::test() {
   std::cout << "Hello from Detector" << std::endl;
--- externals/tensorflow_lite.cmake
+++ externals/tensorflow_lite.cmake
@@ -7,5 +7,10 @@ IF (NOT EXISTS ${EXTERNAL_INSTALL_DIR}/tensorflow_lite)
 ENDIF()
 
 # Provide pathes to the installed dependency, these are available in root scope as there are only "include()" used for dependencies
+SET(TENSORFLOW_LITE_LIB_X86_DIR ${EXTERNAL_INSTALL_DIR}/tensorflow_lite/lib/linux_x86_64/lib)
+SET(TENSORFLOW_LITE_LIB_ARM_DIR ${EXTERNAL_INSTALL_DIR}/tensorflow_lite/lib/aarch64_armv8-a/lib)
+
 SET(TENSORFLOW_LITE_INCLUDE_DIR ${EXTERNAL_INSTALL_DIR}/tensorflow_lite/include)
-SET(GTEST_LIB_DIR ${EXTERNAL_INSTALL_DIR}/tensorflow_lite/lib)
+SET(TENSORFLOW_LITE_DEPENDENCIES_INCLUDE_DIR 
+  ${EXTERNAL_INSTALL_DIR}/tensorflow_lite/include/tensorflow/lite/tools/make/downloads/flatbuffers/include
+)
