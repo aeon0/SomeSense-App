dd1f4e61c3fa3e03c4d845aedac50138580f6308
j-o-d-o <jdobler@protonmail.com>
2019-12-03 21:16:48 +0100

    set up detector component

---
+++ components/object_detection/detector.cpp
@@ -0,0 +1,7 @@
+#include "detector.h"
+#include <iostream>
+
+
+void object_detection::Detector::test() {
+  std::cout << "Hello from Detector" << std::endl;
+}
---
+++ components/object_detection/detector.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace object_detection {
+  class Detector {
+  public:
+    void test();
+  };
+}
