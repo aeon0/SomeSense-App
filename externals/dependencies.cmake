62358ca267d09f12069d8eda39bb2fbbd16b17dc
j-o-d-o <jdobler@protonmail.com>
2019-12-07 10:44:38 +0100

    comment

--- externals/dependencies.cmake
+++ externals/dependencies.cmake
@@ -4,7 +4,7 @@ include(ExternalProject)
 set(OPENCV_VERSION 4.1.2)
 include(externals/opencv.cmake)
 
-set(TENSORFLOW_LITE_VERSION 2.0.0)
+# set(TENSORFLOW_LITE_VERSION 2.0.0), just using master branch for now
 include(externals/tensorflow_lite.cmake)
 
 if(BUILD_TEST)
