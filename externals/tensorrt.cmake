e061fee6f224f1c94734eb00c376c9f8c33b63f1
j-o-d-o <jdobler@protonmail.com>
2019-12-22 10:49:10 +0100

    new tensorrt installation (manuall now)

--- externals/tensorrt.cmake
+++ externals/tensorrt.cmake
@@ -1,22 +1,12 @@
-IF (NOT EXISTS ${EXTERNAL_INSTALL_DIR}/tensorrt)
-  ExternalProject_Add(TensorRTPrj
-    GIT_REPOSITORY "https://github.com/NVIDIA/TensorRT.git"
-    GIT_TAG "${TENSORRT_VERSION}"
-    CMAKE_ARGS
-      -DTRT_LIB_DIR=$ENV{TRT_RELEASE}/lib
-      -DTRT_BIN_DIR=${EXTERNAL_INSTALL_DIR}/tensorrt/out
-      -DBUILD_SAMPLES=OFF
-      -DCMAKE_INSTALL_PREFIX=${EXTERNAL_INSTALL_DIR}/tensorrt
-    INSTALL_COMMAND make -j4 install
-  )
-ENDIF()
+# TensorRT is installed seperatly as it takes some different actions on Jetson Nano and ARM vs General GPU and x86
+# Check the README.md for details on how to install it
 
 add_library(libnvonnxparser SHARED IMPORTED)
-set_property(TARGET libnvonnxparser PROPERTY IMPORTED_LOCATION ${EXTERNAL_INSTALL_DIR}/tensorrt/lib/libnvonnxparser.so)
+set_property(TARGET libnvonnxparser PROPERTY IMPORTED_LOCATION $ENV{TRT_RELEASE}/lib/libnvonnxparser.so)
 add_library(libnvinfer_plugin SHARED IMPORTED)
-set_property(TARGET libnvinfer_plugin PROPERTY IMPORTED_LOCATION ${EXTERNAL_INSTALL_DIR}/tensorrt/lib/libnvinfer_plugin.so)
+set_property(TARGET libnvinfer_plugin PROPERTY IMPORTED_LOCATION $ENV{TRT_RELEASE}/lib/libnvinfer_plugin.so)
 add_library(libnvinfer SHARED IMPORTED)
 set_property(TARGET libnvinfer PROPERTY IMPORTED_LOCATION $ENV{TRT_RELEASE}/lib/libnvinfer.so)
 
 SET(TENSORRT_SHARED_LIBS libnvonnxparser libnvinfer_plugin libnvinfer)
-SET(TENSORRT_INCLUDE_DIRS ${EXTERNAL_INSTALL_DIR}/tensorrt/include)
+SET(TENSORRT_INCLUDE_DIRS $ENV{TRT_RELEASE}/include)
