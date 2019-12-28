5102bebc3a27a954261d33b9e0ee4ba55b5f3ef1
j-o-d-o <jdobler@protonmail.com>
2019-12-28 10:06:31 +0100

    fix key bug in sensor storage

--- components/data_reader/sensor_storage.cpp
+++ components/data_reader/sensor_storage.cpp
@@ -26,21 +26,21 @@ void data_reader::SensorStorage::initFromConfig(const std::string& filepath) {
   }
   for (const auto it: jsonSensorConfig["cams"]) {
     const auto typeName = it["type"].get<std::string>();
+    const auto camName = it["name"].get<std::string>();
     if (typeName == "video") {
-      const std::string sensorKey = it["name"];
-      const std::string filePath = jsonSensorConfig["basepath"].get<std::string>() + "/" + sensorKey + ".mp4";
-      if (timestampsJson.contains(sensorKey)) {
-        auto timestamps = timestampsJson[sensorKey].get<std::vector<int64>>();
-        std::unique_ptr<ICam> videoCam(new VideoCam(filePath, sensorKey, timestamps));
-        addCam(videoCam);
+      const std::string filePath = jsonSensorConfig["basepath"].get<std::string>() + "/" + camName + ".mp4";
+      if (timestampsJson.contains(camName)) {
+        auto timestamps = timestampsJson[camName].get<std::vector<int64>>();
+        std::unique_ptr<ICam> videoCam(new VideoCam(filePath, camName, timestamps));
+        addCam(videoCam, camName);
       }
       else {
-        std::unique_ptr<ICam> videoCam(new VideoCam(filePath, sensorKey));
-        addCam(videoCam);
+        std::unique_ptr<ICam> videoCam(new VideoCam(filePath, camName));
+        addCam(videoCam, camName);
       }
     }
     else if (typeName == "usb") {
-      std::unique_ptr<ICam> usbCam(new UsbCam(it["deviceIdx"].get<int>(), it["name"]));
+      std::unique_ptr<ICam> usbCam(new UsbCam(it["deviceIdx"].get<int>(), camName));
       addCam(usbCam);
     }
     else {
@@ -50,7 +50,7 @@ void data_reader::SensorStorage::initFromConfig(const std::string& filepath) {
 }
 
 std::string data_reader::SensorStorage::addCam(std::unique_ptr<ICam>& cam, std::string camKey) {
-  if (camKey != "") {
+  if (camKey == "") {
     camKey = cam->getName() + "_" + std::to_string(_sensorCounter);
   }
   _cams.insert({camKey, std::move(cam)});
