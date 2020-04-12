541bd89ccb2f5db941e8f692f5004ad630c2a078
j-o-d-o <jdobler@protonmail.com>
2020-04-12 10:00:04 +0200

    first basic implementation for carla

--- components/data_reader/cams/carla.cpp
+++ components/data_reader/cams/carla.cpp
@@ -2,14 +2,6 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include <carla/client/Client.h>
-#include <carla/client/Map.h>
-#include <carla/client/Sensor.h>
-#include <carla/client/World.h>
-#include <carla/geom/Transform.h>
-#include <carla/image/ImageIO.h>
-#include <carla/image/ImageView.h>
-#include <carla/sensor/data/Image.h>
 
 
 data_reader::Carla::Carla(const std::string name, const TS& algoStartTime):
@@ -18,33 +10,76 @@ data_reader::Carla::Carla(const std::string name, const TS& algoStartTime):
   const std::string host = "localhost";
   const uint16_t port = 2000;
   auto client= carla::client::Client(host, port);
+  std::cout << "Connecting to Carla..." << std::endl;
   client.SetTimeout(10s);
+  std::cout << "Success: Carla Connection" << std::endl;
 
   auto availableMaps = client.GetAvailableMaps();
-  // std::cout << "Loading world: " << town_name << std::endl;
+  auto townName = availableMaps[0];
+  std::cout << "Loading world: " << townName << std::endl;
+  auto world = client.LoadWorld(townName);
+  auto map = world.GetMap();
+  auto transform = map->GetRecommendedSpawnPoints()[0];
+  auto blueprintLib = world.GetBlueprintLibrary();
+  auto vehicles = blueprintLib->Filter("vehicle");
+  auto blueprint = vehicles->at(0);
+  auto actor = world.SpawnActor(blueprint, transform);
+  _egoVehicle = boost::static_pointer_cast<carla::client::Vehicle>(actor);
 
-  // auto world = client.LoadWorld(town_name);
-  // auto transform = RandomChoice(map->GetRecommendedSpawnPoints(), rng);
+  // Apply control to vehicle
+  carla::client::Vehicle::Control control;
+  control.throttle = 1.0f;
+  _egoVehicle->ApplyControl(control);
 
-  // _frameRate = _cam.get(cv::CAP_PROP_FPS);
-  // _frameSize = cv::Size(_cam.get(cv::CAP_PROP_FRAME_WIDTH), _cam.get(cv::CAP_PROP_FRAME_HEIGHT));
+  // Move spectator so we can see the vehicle from the simulator window.
+  auto spectator = world.GetSpectator();
+  transform.location += 32.0f * transform.GetForwardVector();
+  transform.location.z += 2.0f;
+  transform.rotation.yaw += 180.0f;
+  transform.rotation.pitch = -15.0f;
+  spectator->SetTransform(transform);
 
-  // Start thread to read image and store it into _currFrame
-  std::thread dataReaderThread(&data_reader::Carla::readData, this);
-  dataReaderThread.detach();
+  // Find a rgb camera, set params
+  auto cameraBpPtr = blueprintLib->Find("sensor.camera.rgb");
+  assert(cameraBpPtr != nullptr && "Carla: Can not find sensor blueprint");
+  auto cameraBp = static_cast<carla::client::ActorBlueprint>(*(cameraBpPtr));
+  cameraBp.SetAttribute("fov", "120");
+  cameraBp.SetAttribute("image_size_x", "1280");
+  cameraBp.SetAttribute("image_size_y", "720");
+  cameraBp.SetAttribute("sensor_tick", "0.02"); // 50 fps
+  _frameRate = 50;
+  _frameSize = cv::Size(1280, 720);
+
+  // Spawn a camera attached to the vehicle.
+  auto cameraTransform = carla::geom::Transform {
+      carla::geom::Location{-5.5f, 0.0f, 2.8f},   // x, y, z.
+      carla::geom::Rotation{-15.0f, 0.0f, 0.0f}}; // pitch, yaw, roll.
+  auto camActor = world.SpawnActor(cameraBp, cameraTransform, actor.get());
+  _rgbCamera = boost::static_pointer_cast<carla::client::Sensor>(camActor);
+
+  _rgbCamera->Listen(std::bind(&data_reader::Carla::readRgbCameraData, this, std::placeholders::_1));
 }
 
-void data_reader::Carla::readData() {
-  for (;;) {
-    const auto captureTime = std::chrono::high_resolution_clock::now();
+data_reader::Carla::~Carla() {
+  std::cout << "Destruct Data from Carla" << std::endl;
+  _egoVehicle->Destroy();
+  _rgbCamera->Destroy();
+}
 
-    // bool success = _cam.read(_bufferFrame);
+void data_reader::Carla::readRgbCameraData(carla::SharedPtr<carla::sensor::SensorData> sensorData) {
+  const auto captureTime = std::chrono::high_resolution_clock::now();
 
-    std::lock_guard<std::mutex> lockGuard(_readMutex);
-    _currTs = static_cast<int64_t>(std::chrono::duration<double, std::micro>(captureTime - _algoStartTime).count());
-    _currFrameNr += 1;
-    _currFrame = _bufferFrame.clone();
-    _bufferFrame.release();
-    _validFrame = true;
-  }
+  auto imagePtr = boost::static_pointer_cast<carla::sensor::data::Image>(sensorData);
+  assert(imagePtr != nullptr && "Carla: Error getting image data from rgb sensor");
+  auto width = imagePtr->GetWidth();
+  auto height = imagePtr->GetHeight();
+  cv::Mat bufferFrame(height, width, CV_8UC4, imagePtr->begin());
+  cv::cvtColor(bufferFrame, bufferFrame, cv::COLOR_BGRA2BGR);
+
+  std::lock_guard<std::mutex> lockGuard(_readMutex);
+  _currTs = static_cast<int64_t>(std::chrono::duration<double, std::micro>(captureTime - _algoStartTime).count());
+  _currFrameNr += 1;
+  _currFrame = bufferFrame.clone();
+  bufferFrame.release();
+  _validFrame = true;
 }
--- components/data_reader/cams/carla.h
+++ components/data_reader/cams/carla.h
@@ -2,6 +2,16 @@
 
 #include <tuple>
 #include "base_cam.h"
+#include <carla/client/Sensor.h>
+#include <carla/client/ActorBlueprint.h>
+#include <carla/client/BlueprintLibrary.h>
+#include <carla/client/Client.h>
+#include <carla/client/Map.h>
+#include <carla/client/World.h>
+#include <carla/geom/Transform.h>
+#include <carla/image/ImageIO.h>
+#include <carla/image/ImageView.h>
+#include <carla/sensor/data/Image.h>
 
 
 using namespace std::chrono_literals;
@@ -10,8 +20,12 @@ namespace data_reader {
   class Carla : public BaseCam {
   public:
     Carla(const std::string name, const TS& algoStartTime);
+    ~Carla();
 
   private:
-    void readData();
+    void readRgbCameraData(carla::SharedPtr<carla::sensor::SensorData> sensorData);
+
+    boost::shared_ptr<carla::client::Sensor> _rgbCamera;
+    boost::shared_ptr<carla::client::Vehicle> _egoVehicle;
   };
 }
