ac20eaaa4208b4c406911ce8aea10386ee139c9a
j-o-d-o <jdobler@protonmail.com>
2020-04-17 10:11:09 +0200

    add c++17 flag

--- clang_toolchain.cmake
+++ clang_toolchain.cmake
@@ -2,4 +2,4 @@
 # this file is included in CMakeLists.txt in the root folder
 set(CMAKE_C_COMPILER /usr/bin/clang-8)
 set(CMAKE_CXX_COMPILER /usr/bin/clang++-8)
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1z -pthread")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -pthread")
