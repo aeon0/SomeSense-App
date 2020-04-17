f2f26e74ef0f70ca9212daf8a94e0cc465f8d417
j-o-d-o <jdobler@protonmail.com>
2020-04-17 10:19:24 +0200

    toolchain adapt

--- clang_toolchain.cmake
+++ clang_toolchain.cmake
@@ -2,4 +2,8 @@
 # this file is included in CMakeLists.txt in the root folder
 set(CMAKE_C_COMPILER /usr/bin/clang-8)
 set(CMAKE_CXX_COMPILER /usr/bin/clang++-8)
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -pthread")
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -pthread")
+message("===== ${CMAKE_CXX_FLAGS} ===")
+
