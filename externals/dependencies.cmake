a7fd3b9a8df93e4570e1c8deb115e75a6436ce0f
Jo <jdobler@protonmail.com>
2020-05-01 14:26:59 +0200

    removing fbs

--- externals/dependencies.cmake
+++ externals/dependencies.cmake
@@ -7,9 +7,6 @@ include(externals/opencv.cmake)
 # Note: Currently this is installed via dependencis.sh, this is currently just to find the package in cmake
 include(externals/capnproto.cmake)
 
-set(FLATBUFFERS_VERSION v1.12.0)
-include(externals/flatbuffers.cmake)
-
 if(BUILD_SIM)
   # version also must be changed for the server on the download in ./scripts/dependencies.sh
   set(CARLA_VERSION 0.9.7)
