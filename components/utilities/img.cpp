f750943867d720bde952a2d8277e9b2abdea8ec6
j-o-d-o <jdobler@protonmail.com>
2021-01-19 17:50:11 +0100

    crop every possible aspect ratio

--- components/utilities/img.cpp
+++ components/utilities/img.cpp
@@ -2,22 +2,38 @@
 
 
 util::img::Roi util::img::cropAndResize(const cv::Mat& inputMat, cv::Mat& outputMat, int targetHeight, int targetWidth, int offsetBottom) {
-  double currRatio = inputMat.size().width / static_cast<double>(inputMat.size().height);
-  double targetRatio = targetWidth / static_cast<double>(targetHeight);
-  int deltaHeight = inputMat.size().height - (inputMat.size().width / targetRatio);
-  int deltaHeightTop = deltaHeight - offsetBottom;
-  cv::Rect cutOut;
-  cutOut.x = 0;
-  cutOut.y = deltaHeightTop;
-  cutOut.width = inputMat.size().width;
-  cutOut.height = inputMat.size().height - deltaHeight;
-  cv::Mat croppedImg = inputMat(cutOut);
-  cv::resize(croppedImg, outputMat, cv::Size(targetWidth, targetHeight));
+  // First lets cut the offsetBottom
+  cv::Rect cutOffsetBottom;
+  cutOffsetBottom.x = 0;
+  cutOffsetBottom.y = 0;
+  cutOffsetBottom.width = inputMat.size().width;
+  cutOffsetBottom.height = inputMat.size().height - offsetBottom;
+  cv::Mat offsetInputMat = inputMat(cutOffsetBottom);
 
+  // Adjust to aspect ratio by either removing from left/right or removing from top of image
+  double currRatio = offsetInputMat.size().width / static_cast<double>(offsetInputMat.size().height);
+  double targetRatio = targetWidth / static_cast<double>(targetHeight);
+  cv::Rect cutOut;
   Roi roi;
-  roi.offsetLeft = 0;
-  roi.offsetTop = deltaHeightTop;
-  roi.scale = targetWidth / float(inputMat.size().width);
+  if (currRatio > targetRatio) {
+    const int deltaWidth = -int((targetRatio * offsetInputMat.size().height) - offsetInputMat.size().width);
+    roi.offsetLeft = int(deltaWidth * 0.5);
+    cutOut.x = roi.offsetLeft;
+    cutOut.y = 0;
+    cutOut.width = offsetInputMat.size().width - deltaWidth;
+    cutOut.height = offsetInputMat.size().height;
+    roi.scale = targetHeight / float(offsetInputMat.size().height);
+  }
+  else {
+    roi.offsetTop = offsetInputMat.size().height - (offsetInputMat.size().width / targetRatio);
+    cutOut.x = 0;
+    cutOut.y = roi.offsetTop;
+    cutOut.width = offsetInputMat.size().width;
+    cutOut.height = offsetInputMat.size().height - roi.offsetTop;
+    roi.scale = targetWidth / float(inputMat.size().width);
+  }
+  cv::Mat croppedImg = offsetInputMat(cutOut);
+  cv::resize(croppedImg, outputMat, cv::Size(targetWidth, targetHeight));
 
   return roi;
 }
